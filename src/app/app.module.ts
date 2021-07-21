ce3fc87d0352f18adf35fb50de5d08139d7f27c2
leanneminnock <lelannem91@gmail.com>
2021-07-21 15:41:10 +0100

    - created PokeList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokeListComponent
   ],
   imports: [
     BrowserModule,
