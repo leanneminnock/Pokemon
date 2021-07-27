77669c097a45220b7ff5d4cad1c4d380baaabb72
leanneminnock <lelannem91@gmail.com>
2021-07-27 16:10:09 +0100

    - configured routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokeListComponent } from './poke-list/poke-list.component';
+import { PokemonDetailedViewComponent } from './pokemon-detailed-view/pokemon-detailed-view.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: "",
+    component: PokeListComponent
+  },
+  {
+    path: "pokemon/:pokeName",
+    component: PokemonDetailedViewComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
