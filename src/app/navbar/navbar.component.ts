3df2d68018d512f966101c3161c7efa9c681c99e
leanneminnock <leanneminnock91@gmail.com>
2021-08-04 20:23:15 +0100

    configured navbar component
    - added types dropdown using getAllTypes() to populate the list

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataHandlerService } from '../data-handler.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public data: DataHandlerService) { }
 
   ngOnInit(): void {
+    this.data.getAllTypes();
   }
 
 }
