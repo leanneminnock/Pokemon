d5f46d7a725ae97ba48b2d94a1f2e6f43c625cf2
leanneminnock <leanneminnock91@gmail.com>
2021-08-04 20:53:25 +0100

    - configured route for types component redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokeListComponent } from './poke-list/poke-list.component';
+import { PokeTypesComponent } from './poke-types/poke-types.component';
 import { PokemonDetailedViewComponent } from './pokemon-detailed-view/pokemon-detailed-view.component';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: ":pokeName",
     component: PokeListComponent
+  },
+  {
+    path: "type/:pokeType",
+    component: PokeTypesComponent
   },
 ];
 @NgModule({
