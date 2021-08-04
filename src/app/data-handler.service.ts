401dae8e6b70074041e8849e7cabdc564cb1557d
leanneminnock <leanneminnock91@gmail.com>
2021-08-04 20:23:48 +0100

    - added getAllTypes() API request

--- src/app/data-handler.service.ts
+++ src/app/data-handler.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GetAllPokemonResponse } from './modals/get-all-pokemon-response';
+import { GetAllTypesResponse } from './modals/get-all-types-response';
 import { Pokemon } from './modals/pokemon';
 
 @Injectable({
@@ -8,8 +9,10 @@ import { Pokemon } from './modals/pokemon';
 })
 export class DataHandlerService {
   private _pokemonUrl = "https://pokeapi.co/api/v2/pokemon/";
+  private _typeUrl = "https://pokeapi.co/api/v2/type/";
   public AllPokemon: GetAllPokemonResponse | null = null;
   public pokemon: Pokemon | undefined;
+  public allTypes: GetAllTypesResponse | null = null; 
 
   constructor(private http: HttpClient) { }
 
@@ -19,6 +22,10 @@ export class DataHandlerService {
 
   public getPokemonByName(name: string){
    return this.http.get<Pokemon>(this._pokemonUrl + name);
-  }        
+  }   
+  
+  public getAllTypes(offset: number = 0, limit: number = 30){
+    this.http.get<GetAllTypesResponse>(this._typeUrl + `?offset=${offset}&limit=${limit}`).subscribe(data => this.allTypes = data);
+  }
 }
 
