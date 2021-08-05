984b30f4f2b203df009a52f7670f3b45d1828c97
leanneminnock <leanneminnock91@gmail.com>
2021-08-05 18:02:40 +0100

    Poke Type Component set up
    - set up template to reneder data
    - configured paramMap to subscribe and configure data for component
    - configured routing to make all list items interactible

--- src/app/poke-types/poke-types.component.ts
+++ src/app/poke-types/poke-types.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataHandlerService } from '../data-handler.service';
+import { PokeTypeInfo } from '../modals/poke-type';
 
 @Component({
   selector: 'app-poke-types',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poke-types.component.css']
 })
 export class PokeTypesComponent implements OnInit {
+  public pokemonType: PokeTypeInfo | undefined;
 
-  constructor() { }
+  constructor(public data: DataHandlerService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const pType = params.get('pokeType');
+      if(pType)
+      {
+        this.data.getPokemonByType(pType).subscribe(data => this.pokemonType = data);
+      }
+    });
   }
 
 }
