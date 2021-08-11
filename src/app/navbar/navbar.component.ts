647f6e47169b7ef87d83ba3f22092355a62d019d
leanneminnock <leanneminnock91@gmail.com>
2021-08-11 15:21:13 +0100

    - add bootstrap classes to customise site look

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,9 +9,18 @@ import { DataHandlerService } from '../data-handler.service';
 export class NavbarComponent implements OnInit {
 
   constructor(public data: DataHandlerService) { }
+  public pokeName: string | undefined;
+  
 
   ngOnInit(): void {
     this.data.getAllTypes();
+  }
+
+  public onSearch(): void{
+    if(this.pokeName){
+       this.data.getPokemonByName(this.pokeName);
+    }
+    this.pokeName = "";
   }
 
 }
